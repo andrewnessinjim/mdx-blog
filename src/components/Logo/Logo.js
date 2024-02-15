a2eafc67aac559208ca478fd6133f8c859f8a821
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-02-15 10:25:26 +0530

    Use same font throughout

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
-import { Comic_Neue } from 'next/font/google';
 import { BLOG_TITLE } from '@/constants';
 
 import styles from './Logo.module.css';
-
-const comicNeue = Comic_Neue({
-  subsets: ["latin"],
-  weight: ["700"]
-})
 
 function Logo({ mobileAlignment = 'left' }) {
   return (
     <Link
       href="/"
-      className={styles.wrapper + " " +comicNeue.className}
+      className={styles.wrapper}
       data-mobile-alignment={mobileAlignment}
     >
       {BLOG_TITLE}
